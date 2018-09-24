9d1454c1c953c04eab1e913f22bdf496b253e22f
Spencer Parkin <stparkin@mmm.com>
2018-09-24 15:31:16 -0600

    linearly dependent blades are zero

--- manipulators/outer_product_handler.py
+++ manipulators/outer_product_handler.py
@@ -9,6 +9,12 @@ class OuterProductHandler(MathTreeManipulator):
     def _manipulate_subtree(self, node):
         scalar_list, vector_list = self._parse_blade(node)
         if scalar_list is not None and vector_list is not None:
+            for i in range(len(vector_list)):
+                vector_a = vector_list[i]
+                for j in range(i + 1, len(vector_list)):
+                    vector_b = vector_list[j]
+                    if vector_a.data == vector_b.data:
+                        return MathTreeNode(0.0)
             adjacent_swap_count = self._sort_list(vector_list, sort_key=lambda vector: vector.data)
             if adjacent_swap_count > 0:
                 new_node = MathTreeNode('^', scalar_list + vector_list)
